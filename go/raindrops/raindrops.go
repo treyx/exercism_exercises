54bb9d7aaa487a34dbb01e21dcc942d7d2768d4a
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-02 10:44:20 -0600

    ran go fmt for reformatting

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -4,13 +4,13 @@ import "strconv"
 
 func Convert(number int) string {
 	new_string := ""
-	if number % 3 == 0 {
+	if number%3 == 0 {
 		new_string += "Pling"
 	}
-	if number % 5 == 0 {
+	if number%5 == 0 {
 		new_string += "Plang"
 	}
-	if number % 7 == 0 {
+	if number%7 == 0 {
 		new_string += "Plong"
 	}
 	if len(new_string) == 0 {
