cee51c480b98ee3a8c2c8168e6780c7ad93d28d5
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-04-10 10:57:09 -0600

    go/clock iteration 1 passing

---
+++ go/clock/clock.go
@@ -0,0 +1,29 @@
+package clock
+
+import "fmt"
+
+type Clock int
+
+const TestVersion = 2
+const minutesPerDay = 1440
+const munitesPerHour = 60
+
+func Time(hour, minutes int) Clock {
+	c := Clock((hour*munitesPerHour + minutes) % minutesPerDay)
+	if c < 0 {
+		c += minutesPerDay
+	}
+	return c
+}
+
+func (c Clock) Add(minutes int) Clock {
+	c = (c + Clock(minutes)) % minutesPerDay
+	if c < 0 {
+		c += minutesPerDay
+	}
+	return c
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", c/munitesPerHour, c%munitesPerHour)
+}
