497da34ab612f6b9d32d1cb46bb681c401a94921
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-15 21:50:43 -0600

    commiting first python exercise - bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,17 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
 def hey(what):
+    response = {"": "Whatever.",
+                 "question": "Sure.",
+                 "silence": "Fine. Be that way!",
+                 "upper": "Whoa, chill out!"}
 
-    return
+    what = what.strip()
+    state = ""
+
+    if what.endswith("?"):
+        state = "question"
+    if what.isupper():
+        state = "upper"
+    if what == "":
+        state = "silence"
+
+    return response[state]
